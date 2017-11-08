a823d617bc34352f922d7edce6b879b43fc34605
Steven Gibson <gib.screenmedia@gmail.com>
2017-11-08 17:38:59 +0000

    Reminder.

--- src/utils/getProductById.js
+++ src/utils/getProductById.js
@@ -1,6 +1,10 @@
 'use strict';
 
-// Yuck!!!
+/* TODO
+  - gets the full detail for each product available at a given bar
+  - we want product id, name, imageUrl, priceInPence
+  - find a cleaner way to do this as this is unreadable
+*/
 export default function getProductById(products, barsProducts, productId, barId) {
   let product = products.find((product) => product.id === productId);
   product.priceInPence = barsProducts.find((barsProduct) => barsProduct.productId === productId && barsProduct.barId === barId).priceInPence;
